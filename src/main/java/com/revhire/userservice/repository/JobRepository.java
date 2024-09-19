e93dd5a4d840c0140379dd7a49aeb01f4096a3f7
Akash-psg <akashpsgct01@gmail.com>
2024-09-19 21:58:41 +0530

    files added

--- src/main/java/com/revhire/userservice/repository/JobRepository.java
+++ src/main/java/com/revhire/userservice/repository/JobRepository.java
@@ -1,7 +1,5 @@
 package com.revhire.userservice.repository;
 
-
-import com.revhire.userservice.enums.ExperienceRequired;
 import com.revhire.userservice.models.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,19 +10,6 @@ import java.util.List;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
-
-    @Query("SELECT j FROM Job j WHERE " +
-            "(:jobTitle IS NULL OR j.jobTitle LIKE %:jobTitle%) AND " +
-            "(:location IS NULL OR j.location LIKE %:location%) AND " +
-            "(:experienceRequired IS NULL OR j.experienceRequired = :experienceRequired) AND " +
-            "(:skillsRequired IS NULL OR j.skillsRequired LIKE %:skillsRequired%) AND " +
-            "(:companyName IS NULL OR j.companyName LIKE %:companyName%)")
-    List<Job> findJobs(
-            @Param("jobTitle") String jobTitle,
-            @Param("location") String location,
-            @Param("experienceRequired") ExperienceRequired experienceRequired,
-            @Param("skillsRequired") String skillsRequired,
-            @Param("companyName") String companyName);
 
     List<Job> findByEmployer_EmpolyerId(Long employerId);
 
